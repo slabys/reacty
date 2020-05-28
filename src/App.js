53ead359570ecdef98ee4715d65103f0d16475d6
BuildTools <unconfigured@null.spigotmc.org>
2020-05-28 02:22:52 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './assets/css/default.css';
 
+//components
+import Header from './components/header/header';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
         <p>LOL</p>
-          <p>I am good</p>
-      </header>
+        <p>I am good</p>
     </div>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import App from "../../App";
+
+function Header(){
+    return (
+        <header className="Header">
+            <p>Header</p>
+        </header>);
+}
+
+export default Header;
